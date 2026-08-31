b6fdd8c3b49315fc1a4858c8d77a65a83cf5535b
faithconnection03-source <faithconnection03@gmail.com>
2026-08-31 23:50:43 +0530

    Add files via upload

--- api.php
+++ api.php
@@ -1,8 +1,12 @@
 <?php
 header("Access-Control-Allow-Origin: *");
-header("Access-Control-Allow-Headers: Content-Type");
-header("Content-Type: application/json; charset=UTF-8");
+header("Access-Control-Allow-Methods: GET, POST, OPTIONS");
+header("Access-Control-Allow-Headers: Content-Type, Authorization");
 
+if ($_SERVER['REQUEST_METHOD'] == 'OPTIONS') {
+    http_response_code(200);
+    exit();
+}
 // --- Aiven MySQL Database Configuration ---
 $host = "mysql-ce96da-faithconnection03-fe6b.f.aivencloud.com";
 $port = 14165;
